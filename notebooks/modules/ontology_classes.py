3cdbb9b0f867a026d84194a542633ae25e22fa52
Alejandro Velasquez <v.alejo96@gmail.com>
2024-07-09 18:13:55 -0500

    Greatly improve ontology population

--- notebooks/modules/ontology_classes.py
+++ notebooks/modules/ontology_classes.py
@@ -3,9 +3,12 @@ Classes to infer ontology instances from a cognitive function and emotion
 """
 import json
 from json import JSONEncoder
-from owlready2 import get_ontology
+from owlready2 import Ontology
 
-onto = get_ontology('./../ontology/COGAF_Ontology.rdf').load()
+
+def set_ontology(ontology: Ontology):
+    global onto
+    onto = ontology
 
 
 class CogafEncoder(JSONEncoder):
@@ -14,11 +17,11 @@ class CogafEncoder(JSONEncoder):
 
 
 class CogafInstance():
-    def __init__(self, cognitiveFunction: str, emotion: str) -> None:
-
-        self.cognitiveFunction = CognitiveFunction(cognitiveFunction)
-
-        self.emotion = Emotion(emotion)
+    def __init__(self, input_dict: dict) -> None:
+        self.name = input_dict["file"][:-4]
+        self.cognitiveFunction = CognitiveFunction(input_dict)
+        self.emotion = HourglassEmotion(input_dict["emotion"], self.name)
+        self.event = input_dict["event"]
 
     def to_json(self) -> str:
         return json.dumps(self, cls=CogafEncoder, indent=4)
@@ -26,25 +29,42 @@ class CogafInstance():
     def to_dict(self) -> dict:
         return json.loads(self.to_json())
 
+    def populate_ontology(self) -> None:
+        if onto[self.name] is None:
+            onto.Event(self.name, eventDescription=self.event)
+        self.cognitiveFunction.populate(self.name)
+        self.emotion.populate(self.name)
+
+    def infer_components(self):
+        pass
+
 
 class CognitiveFunction():
-    def __init__(self, name) -> None:
-        self.name = name
-        if onto[name] is None:
+    def __init__(self, input_dict) -> None:
+        self.name = input_dict["cognitive_function"]
+        if onto[self.name] is None:
             raise NotImplementedError(
                 "Cognitive Function not defined in ontology")
-        self.isBasicFunction = onto[name].isBasicFunction[0]
+        self.isBasicFunction = onto[self.name].isBasicFunction
         self.activities = []
-        for activity in onto[name].trainedThrough:
+        self.tasks = []
+        self.capabilites = input_dict["capabilities"]
+
+    def populate(self, event_name):
+        for cap in self.capabilites:
+            if onto[cap] is None:
+                onto.Capability(cap)
+
+            onto[self.name].generates.append(onto[cap])
+            onto[cap].inEvent.append(onto[event_name])
+
+    def infer(self):
+
+        for activity in onto[self.name].trainedThrough:
             self.activities.append(ComplementaryActivity(activity.name))
 
-        self.tasks = []
-        for task in onto[name].assessedWith:
+        for task in onto[self.name].assessedWith:
             self.tasks.append(PsychologicalTask(task.name))
-
-        self.capabilites = []
-        for capability in onto[name].generates:
-            self.capabilites.append(capability.name)
 
 
 class ComplementaryActivity():
@@ -76,6 +96,25 @@ class CognitiveTest():
         self.applicationType = instance.applicationType
 
 
+class HourglassEmotion():
+    def __init__(self, emotion, name) -> None:
+        self.name = name + "_emotion"
+        self.introspection = emotion["introspection"]
+        self.temper = emotion["temper"]
+        self.attitude = emotion["attitude"]
+        self.sensitivity = emotion["sensitivity"]
+
+    def populate(self, event_name):
+        if onto[self.name] is None:
+            onto.Emotion(self.name,
+                         introspection=self.introspection["value"],
+                         temper=self.temper["value"],
+                         attitude=self.attitude["value"],
+                         sensitivity=self.sensitivity["value"],
+                         )
+        onto[self.name].inEvent.append(onto[event_name])
+
+
 class Emotion():
     def __init__(self, name) -> None:
         self.name = name
@@ -83,7 +122,7 @@ class Emotion():
             self.isBasicEmotion = None
             self.state = None
             return
-        self.isBasicEmotion = onto[name].isBasicEmotion[0]
+        self.isBasicEmotion = onto[name].isBasicEmotion
         self.state = State(onto[name].hasState)
 
         self.characteristics = []
